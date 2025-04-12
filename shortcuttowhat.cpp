b1f8b1328b552093b37d34dc5dde0f0775eae719
ariemoii <arendspijker@gmail.com>
2025-04-12 15:39:07 +0200

    solved hello and shortcuttowhat

---
+++ shortcuttowhat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  n +=5;
+  n*=3;
+  n-=10;
+  cout << n;
+  return 0;
+}
